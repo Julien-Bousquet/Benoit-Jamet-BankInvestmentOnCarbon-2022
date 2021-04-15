32dc685b4ad1c2dfba11d8b6d06199c65994e92c
Julien-Bousquet <80256842+Julien-Bousquet@users.noreply.github.com>
2021-04-15 12:23:19 +0200

    add export to function star

--- R/wilcoxon.cut.test.R
+++ R/wilcoxon.cut.test.R
@@ -20,18 +20,6 @@ to.vector <- function(col, data){
   }
 }
 
-#' A fonction that transforms pvalues in stars ***
-#' @author Julien Bousquet (2021)
-#' @param p.val the value of the pvalue.
-#' export
-star <- function(p.val){ 
-  p.val <- abs(p.val)
-  if(p.val>0.1)return(' ')
-  if(p.val>0.05)return('.')
-  if(p.val>0.01)return('*')
-  if(p.val>0.001)return('**')
-  return('***')
-}
 #' Indexes around median.
 #'
 #'  Compute index of a proportion of values of group around the median
